5c3a9b6bf90cf807f664da9faf1744aefef57111
Jeff Capuano <jcapuano328@gmail.com>
2016-08-25 07:37:54 -0400

    initial

---
+++ src/res/icons.js
@@ -0,0 +1,19 @@
+module.exports = {
+	"logo": require('./logo.png'),
+	"splash": require('./splash.png'),
+	"menu": require('./hamburger-light.png'),
+	"info": require('./info-light.png'),
+
+	"first": require('./1.png'),
+	"second": require('./2.png'),
+
+	"1": require('./1.png'),
+	"2": require('./2.png'),
+	"3": require('./3.png'),
+	"4": require('./4.png'),
+	"5": require('./5.png'),
+
+	"refreshButton": require('./ic_action_refresh-light.png'),
+	"prevButton":	require('./ic_action_previous_item.png'),
+	"nextButton": require('./ic_action_next_item.png')
+};
